ea20137a531f6707b27fc678a3cdbc62fdba9694
Wagner Ribeiro <wagnerras@gmail.com>
2019-09-18 13:36:27 -0300

    adicionando update task

--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -89,4 +89,45 @@ RSpec.describe 'Task api' do
 
   end
 
+  describe 'PUT /tasks/:id' do
+    let!(:task) { create(:task, user_id: user.id) }
+
+    before do
+      put "/tasks/#{task.id}", params: {task: task_params}.to_json, headers: headers
+    end
+
+    context 'when params are valid' do
+      let(:task_params) { {title: 'New title task'}  }
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'return json for updated task' do
+        expect(json_body[:title]).to eq(task_params[:title])
+      end
+
+      it 'updates task in database' do
+       expect( Task.find_by(title: task_params[:title]) ).not_to be_nil
+      end
+    end
+
+    context 'when params are invalid' do
+      let(:task_params) { { title: ' '  } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns json error for title' do
+        expect(json_body[:errors]).to have_key(:title)
+      end
+
+      it 'does not update task in database' do
+        expect( Task.find_by(title: task_params[:title]) ).to be_nil
+      end
+
+    end
+  end
+
 end

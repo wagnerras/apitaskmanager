5630d522ea7fd2cc45dc80b747810f047a7d35ed
Wagner Ribeiro <wagnerras@gmail.com>
2019-09-29 09:40:19 -0300

    adicionando novo i18n timeformat

--- app/serializers/api/v2/task_serializer.rb
+++ app/serializers/api/v2/task_serializer.rb
@@ -1,14 +1,18 @@
 class Api::V2::TaskSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :title, :description, :done, :deadline, :created_at, :updated_at,
-             :short_description, :is_late
+             :short_description, :is_late, :deadline_to_br
 
 
   def short_description
-    object.description[0..40]
+    object.description[0..40] if object.description.present?
   end
 
   def is_late
     Time.current > object.deadline if object.deadline.present?
+  end
+
+  def deadline_to_br
+   I18n.l(object.deadline, format: :datetime) if object.deadline.present?
   end
 
   belongs_to :user
--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module Apitask
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.i18n.default_locale = :'pt-BR'
   end
 end
